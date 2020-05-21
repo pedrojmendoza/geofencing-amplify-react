759b0ece237c35cecded04f1937ea6271acf9d61
Pedro Mendoza <pedrojmendoza@gmail.com>
2020-05-22 00:32:17 +0200

    adding leaflet

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import { API, graphqlOperation } from 'aws-amplify'
 import { createTodo } from './graphql/mutations'
 import { listTodos } from './graphql/queries'
 import { withAuthenticator } from '@aws-amplify/ui-react'
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import "./App.css";
 
 const initialState = { name: '', description: '' }
+
+export const icon = new Icon({
+  iconUrl: "/skateboarding.svg",
+  iconSize: [25, 25]
+});
 
 const App = () => {
   const [formState, setFormState] = useState(initialState)
@@ -27,53 +35,47 @@ const App = () => {
     } catch (err) { console.log('error fetching todos') }
   }
 
-  async function addTodo() {
-    try {
-      if (!formState.name || !formState.description) return
-      const todo = { ...formState }
-      setTodos([...todos, todo])
-      setFormState(initialState)
-      await API.graphql(graphqlOperation(createTodo, {input: todo}))
-    } catch (err) {
-      console.log('error creating todo:', err)
-    }
-  }
+  const [activeTodo, setActiveTodo] = React.useState(null);
 
   return (
-    <div style={styles.container}>
-      <h2>Amplify Todos</h2>
-      <input
-        onChange={event => setInput('name', event.target.value)}
-        style={styles.input}
-        value={formState.name} 
-        placeholder="Name"
+    <Map center={[45.4, -75.7]} zoom={11}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <input
-        onChange={event => setInput('description', event.target.value)}
-        style={styles.input}
-        value={formState.description}
-        placeholder="Description"
-      />
-      <button style={styles.button} onClick={addTodo}>Create Todo</button>
-      {
-        todos.map((todo, index) => (
-          <div key={todo.id ? todo.id : index} style={styles.todo}>
-            <p style={styles.todoName}>{todo.name}</p>
-            <p style={styles.todoDescription}>{todo.description}</p>
-          </div>
-        ))
-      }
-    </div>
-  )
-}
 
-const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
-  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
+      {todos.map(todo => (
+        <Marker
+          key={todo.id}
+          position={[
+            todo.lon,
+            todo.lat
+          ]}
+          onClick={() => {
+            setActiveTodo(todo);
+          }}
+          icon={icon}
+        />
+      ))}
+
+      {activeTodo && (
+        <Popup
+          position={[
+            activeTodo.lon,
+            activeTodo.lat,
+          ]}
+          onClose={() => {
+            setActiveTodo(null);
+          }}
+        >
+          <div>
+            <h2>{activeTodo.description}</h2>
+          </div>
+        </Popup>
+      )}            
+    </Map>
+
+  )
 }
 
 export default withAuthenticator(App)
